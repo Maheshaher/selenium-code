e8f0c14be44882210bf822781a76703a159dfde2
mahii <maheshaher546@gmail.com>
2026-01-09 11:43:18 +0530

    waitclass

--- src/seleniumPkg/BrowserCurrentUrl.java
+++ src/seleniumPkg/BrowserCurrentUrl.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserCurrentUrl {
     public static void main(String[] args) {
+
+
         WebDriver driver= new ChromeDriver();
 
         driver.get("https://www.opera.com/");
